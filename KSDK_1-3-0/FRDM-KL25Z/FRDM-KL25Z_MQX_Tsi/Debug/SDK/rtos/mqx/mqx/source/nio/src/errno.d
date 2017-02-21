1c8f6c72f2a123591eb6706b2a41443d2d837813
milton <milton86@gmail.com>
2017-02-21 00:34:17 -0300

    delete folder

--- KSDK_1-3-0/FRDM-KL25Z/FRDM-KL25Z_MQX_Tsi/Debug/SDK/rtos/mqx/mqx/source/nio/src/errno.d
+++
@@ -1,2 +0,0 @@
-SDK/rtos/mqx/mqx/source/nio/src/errno.o: \
- /home/milton/Freescale/KSDK1.3/rtos/mqx/mqx/source/nio/src/errno.c
--- KSDK_1-3-0/FRDM-KL25Z/FRDM-KL25Z_MQX_Tsi/Debug/SDK/rtos/mqx/mqx_stdlib/source/stdio/fp_prv.d
+++
@@ -1,5 +0,0 @@
-SDK/rtos/mqx/mqx_stdlib/source/stdio/fp_prv.o: \
- /home/milton/Freescale/KSDK1.3/rtos/mqx/mqx_stdlib/source/stdio/fp_prv.c \
- /home/milton/Freescale/KSDK1.3/rtos/mqx/mqx_stdlib/source/stdio/fp_prv.h
-
-/home/milton/Freescale/KSDK1.3/rtos/mqx/mqx_stdlib/source/stdio/fp_prv.h:
